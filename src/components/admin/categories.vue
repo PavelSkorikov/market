195f398437e19688d46b00db1f859c21b9584012
PavelSkorikov <spg75@yandex.ru>
2019-07-22 23:03:51 +0300

    edit category admin form

--- src/components/admin/categories.vue
+++ src/components/admin/categories.vue
@@ -8,31 +8,17 @@
 
     <!-- categories list -->
     <q-tree
-      :nodes="categories_tree"
+      :nodes="category_tree"
       node-key="label"
+      :selected.sync="selected"
     />
-
-    <q-list bordered separator>
-      <q-item v-for="category in categories" :key="category.id" clickable v-ripple>
-        <q-item-section @click="categoryData=category, form_edit=true">
-          {{category.name}}
-        </q-item-section>
-        <q-item-section top side>
-          <div class="text-grey-8 q-gutter-xs">
-            <q-btn @click="dialog_delete = true, categoryData = category" class="gt-xs" size="12px"
-                   flat
-                   dense round
-                   icon="delete" />
-          </div>
-        </q-item-section>
-      </q-item>
-    </q-list>
 
     <!-- dialog for delete submit -->
     <q-dialog v-model="dialog_delete" persistent>
       <q-card>
         <q-card-section class="row items-center">
-          <span class="q-ml-sm">Вы уверены, что хотите удалить компанию {{categoryData.name}}?</span>
+          <span class="q-ml-sm">Вы уверены, что хотите удалить категорию {{categoryData.name}}
+            и все подкатегории если таковые имеются?</span>
         </q-card-section>
         <!-- Notice v-close-popup -->
         <q-card-actions align="right">
@@ -48,7 +34,7 @@
         <q-bar  class="bg-positive text-white">
           <div>Добавить категорию</div>
           <q-space />
-          <q-btn dense flat icon="close" @click="categoryData={}" v-close-popup></q-btn>
+          <q-btn dense flat icon="close" @click="categoryData={}, selected=null" v-close-popup></q-btn>
         </q-bar>
         <q-card-section class="row items-center">
           <q-form  class="q-gutter-md">
@@ -69,11 +55,6 @@
               v-model="categoryData.description"
               label="Описание"
             />
-            <q-toggle
-              v-model="availability"
-              color="green"
-              label="Доступна"
-            />
             <div>
               <q-btn @click="add" label="Добавить" type="submit" color="primary"/>
               <q-btn @click="name = null, description = null" label="Сброс" type="reset" color="primary" flat
@@ -86,11 +67,11 @@
 
     <!-- form for edit category -->
     <q-dialog v-model="form_edit">
-      <q-card>
+      <q-card style="min-width: 500px">
         <q-bar  class="bg-positive text-white">
-          <div>Редактировать компанию</div>
+          <div>Редактировать категорию</div>
           <q-space />
-          <q-btn dense flat icon="close" @click="categoryData={}" v-close-popup ></q-btn>
+          <q-btn dense flat icon="close" @click="categoryData={}, selected=null" v-close-popup ></q-btn>
         </q-bar>
         <q-card-section class="row items-center">
           <q-form  class="q-gutter-md">
@@ -98,10 +79,12 @@
               square
               filled
               v-model="categoryData.name"
-              label="Название компании"
+              label="Название категории"
               lazy-rules
               :rules="[ val => val && val.length > 0 || 'Пожалуйста введите что нибудь']"
+              style="min-width: 470px"
             />
+            <q-select v-model="categoryData.parent_name" :options="category_names" label="Родительская категория" />
             <q-input
               type = 'textarea'
               square
@@ -111,6 +94,8 @@
             />
             <div>
               <q-btn @click="edit" label="Изменить" type="submit" color="primary"/>
+              <q-btn @click="dialog_delete = true" label="Удалить" type="submit" color="primary" flat
+                     class="q-ml-sm"/>
             </div>
           </q-form>
         </q-card-section>
@@ -127,71 +112,86 @@
     name: 'Categories',
     data() {
       return {
-        model: null,
+        selected: null,
         categoryData: {},
         categories: null,
-        availability: true,
         dialog_delete: false,
         form_add: false,
-        form_edit: false,
-        categories_tree: [
-          {
-            label: 'Satisfied customers (with avatar)',
-            avatar: 'https://cdn.quasar.dev/img/boy-avatar.png',
-            children: [
-              {
-                label: 'Good food (with icon)',
-                icon: 'restaurant_menu',
-                children: [
-                  {label: 'Quality ingredients'},
-                  {label: 'Good recipe'}
-                ]
-              },
-              {
-                label: 'Good service (disabled node with icon)',
-                icon: 'room_service',
-                disabled: true,
-                children: [
-                  {label: 'Prompt attention'},
-                  {label: 'Professional waiter'}
-                ]
-              },
-              {
-                label: 'Good service (disabled node with icon)',
-                icon: 'room_service',
-                disabled: true,
-                children: [
-                  {label: 'Prompt attention'},
-                  {label: 'Professional waiter'}
-                ]
-              }
-            ]
+        form_edit: false
+      }
+    },
+    //with open a component load list categories from server
+    beforeCreate() {
+      axios
+        .get(this.appConfig.api_url + '/getCategory')
+        .then(response => (this.categories = response.data));
+    },
+    watch: {
+      selected: function (newSelected) {
+        this.categoryData.name = newSelected;
+        for(let category of this.categories){
+          if(category.name == newSelected){
+            this.categoryData.description = category.description;
+            this.categoryData.id = category.id;
+            break;
           }
-        ]
+        }
+        this.form_edit = true;
       }
     },
     computed: {
       //computed a list of categories names for form "add category"
       category_names: function () {
-        let names = [];
-        for(let category of this.categories){
-          names.push(category.name);
+        if (this.categories) {
+          let names = [];
+          for (let category of this.categories) {
+            names.push(category.name);
+          }
+          return names;
         }
-        return names;
       },
       //computed a list of tree categories for visualization in page
       category_tree: function () {
-        let tree = [];
-
-
+        if (this.categories) {
+          let tree = [];
+          var i = 0;
+          // create tree with parents categories
+          for (let category of this.categories) {
+            if (!category.parent_name) {
+              tree.push({
+                'label': category.name,
+                'children': []
+              });
+              i += 1;
+            }
+          }
+          // recursive function for add category at tree
+          function find_parent(elem, tree_items) {
+            for (let item of tree_items) {
+              if (item.label == elem.parent_name) {
+                item.children.push({
+                  'label': elem.name,
+                  'children': []
+                });
+                i += 1;
+                return;
+              } else {
+                if (item.children) find_parent(elem, item.children);
+              }
+            }
+          }
+          // for each category from categories create element tree categories
+          while (i < this.categories.length) {
+            for (let category of this.categories) {
+              if (category.parent_name) {
+                find_parent(category, tree);
+              }
+            }
+          }
+          return tree;
+        }
       }
     },
-        //with open a component load list categories from server
-        mounted() {
-          axios
-            .get(this.appConfig.api_url + '/getCategory')
-            .then(response => (this.categories = response.data));
-        },
         methods: {
           //method get list categories from server
           get_categories() {
@@ -204,7 +204,8 @@
             axios
               .delete(this.appConfig.api_url + '/delCategory', {
                 params: {
-                  id: this.categoryData.id
+                  id: this.categoryData.id,
+                  name: this.categoryData.name
                 }
               }).then((res) => {
               console.log('Ответ сервера:', res);
@@ -217,15 +218,10 @@
           // method - create category
           add() {
             if (this.categoryData.name) {
-              console.log(this.categoryData.name);
-              console.log(this.categoryData.description);
-              console.log(this.categoryData.parent_name);
-              console.log(this.availability);
               axios.post(this.appConfig.api_url + '/addCategory', {
                   name: this.categoryData.name,
                   description: this.categoryData.description,
-                  parent_name: this.categoryData.parent_name,
-                  availability: this.availability
+                  parent_name: this.categoryData.parent_name
               }).then((res) => {
                 console.log('Ответ сервера:', res);
                 if (res.status == 200) {
@@ -247,12 +243,16 @@
           // method - edit category
           edit() {
             if (this.categoryData.name) {
+              console.log(this.categoryData.name);
+              console.log(this.categoryData.description);
+              console.log(this.categoryData.parent_name);
               console.log(this.categoryData.id);
               axios
-                .put(this.appConfig.api_url + '/putCompany', {
+                .put(this.appConfig.api_url + '/putCategory', {
                   id: this.categoryData.id,
                   name: this.categoryData.name,
-                  description: this.categoryData.description
+                  description: this.categoryData.description,
+                  parent_name: this.categoryData.parent_name,
                 }).then((res) => {
                 console.log('Ответ сервера:', res);
                 if (res.status == 204) {
